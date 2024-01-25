91dc9bd3620ebc8a6274a1996208cc6a2ef47d05
raphaelmelo <git@velodigital.com.br>
2024-01-25 17:48:33 -0300

    allow iframe

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 // next.config.js
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      net: false,
+      os: false,
+      tls: false,
+      fs: false,
+      path: false,
+      child_process: false,
+      canvas: false,
+    };
+    return config;
+  },
   async headers() {
     return [
       {
@@ -15,3 +30,5 @@ module.exports = {
     ];
   },
 };
+ 
+export default nextConfig;

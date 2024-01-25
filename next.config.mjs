3f6ba68095c6376e9a6082a805efe07c273da928
raphaelmelo <git@velodigital.com.br>
2024-01-25 11:32:03 -0300

    Component lista de cartoes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: config => {
-      config.resolve.fallback = { ...config.resolve.fallback, net: false, os: false, tls: false, fs: false, path: false, child_process: false, canvas: false };
-      return config;
-    },
-  }
-  
-  export default nextConfig
+  reactStrictMode: true,
+  webpack: config => {
+     config.resolve.fallback = { ...config.resolve.fallback, net: false, os: false, tls: false, fs: false, path: false, child_process: false, canvas: false };
+     return config;
+  },
+  async headers() {
+     return [
+       {
+         // Aplica a todas as páginas
+         source: "/(.*)",
+         headers: [
+           {
+             key: "X-Frame-Options",
+             value: "ALLOWALL",
+           },
+         ],
+       },
+     ];
+  },
+ }
+ 
+ export default nextConfig

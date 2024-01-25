8345310d0569c3754d90abb80ced5b8158320210
raphaelmelo <git@velodigital.com.br>
2024-01-25 12:26:31 -0300

    allow iframe

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,24 @@
-module.exports = {
- async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors *",
-          },
-        ],
-      },
-    ];
- },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: config => {
+     config.resolve.fallback = { ...config.resolve.fallback, net: false, os: false, tls: false, fs: false, path: false, child_process: false, canvas: false };
+     return config;
+  },
+  async headers() {
+     return [
+       {
+         // Aplica a todas as páginas
+         source: "/(.*)",
+         headers:[
+        { "key": "Access-Control-Allow-Credentials", "value": "true" },
+        { "key": "Access-Control-Allow-Origin", "value": "*" },
+        { "key": "Access-Control-Allow-Methods", "value": "GET,DELETE,PATCH,POST,PUT" },
+        { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+      ]
+       },
+     ];
+  },
+ }
+ 
+ export default nextConfig

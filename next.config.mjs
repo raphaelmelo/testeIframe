bb12df154d7f02b10574faebf9dabefb3c9b78f1
raphaelmelo <git@velodigital.com.br>
2024-01-25 12:24:52 -0300

    allow iframe

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,16 @@
 module.exports = {
-  async headers() {
-     return [
-       {
-         // Apply these headers to all routes in your application.
-         source: '/(.*)',
-         headers: [
-           {
-             key: 'Content-Security-Policy',
-             value: "frame-ancestors *",
-           },
-         ],
-       },
-     ];
-  },
- };
- 
+ async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *",
+          },
+        ],
+      },
+    ];
+ },
+};

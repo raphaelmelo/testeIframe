b136c2f8a2699ee5c63d6524acacd2ca247869fe
raphaelmelo <git@velodigital.com.br>
2024-01-25 11:56:16 -0300

    Add middleware allow csp

--- _middleware.js
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export function middleware(req) {
- return new NextResponse().withHeader('Access-Control-Allow-Origin', '*')
-}
--- next.config.mjs
+++ next.config.mjs
@@ -10,12 +10,12 @@ const nextConfig = {
        {
          // Aplica a todas as páginas
          source: "/(.*)",
-         headers: [
-           {
-             key: "X-Frame-Options",
-             value: "ALLOWALL",
-           },
-         ],
+         headers:[
+          { "key": "Access-Control-Allow-Credentials", "value": "true" },
+          { "key": "Access-Control-Allow-Origin", "value": "*" },
+          { "key": "Access-Control-Allow-Methods", "value": "GET,DELETE,PATCH,POST,PUT" },
+          { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+        ]
        },
      ];
   },
